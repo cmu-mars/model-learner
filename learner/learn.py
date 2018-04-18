7fc0cef5a3b63d587a14d294d9caee0abbe026c0
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2018-04-18 18:51:17 -0400

    Added config list generation based on true model

---
+++ learner/learn.py
@@ -0,0 +1,107 @@
+import numpy as np
+import mlearner
+import model
+import os
+import itertools
+import json
+
+# the main for learning
+
+from learner.mlearner import MLearner
+from learner.model import genModelTermsfromString, Model, genModelfromCoeff
+from ready_db import ReadyDB
+from lib import *
+
+model_path = os.path.expanduser("~/cp1/models/")
+learned_model_path = os.path.expanduser("~/cp1/")
+config_list_file = os.path.expanduser('~/cp1/config_list.json')
+config_list_file_true = os.path.expanduser('~/cp1/config_list_true.json')
+ready_json = os.path.expanduser("~/ready")
+learned_model_name = 'learned_model'
+
+ndim = 20
+test_size = 10000
+mu, sigma = 0, 0.1
+
+
+ready = ReadyDB(ready_db=ready_json)
+budget = ready.get_budget()
+model_name = ready.get_power_model()
+
+
+default_conf = np.reshape(np.ones(ndim), (1, ndim))
+
+with open(os.path.join(model_path, model_name), 'r') as model_file:
+    model_txt = model_file.read()
+
+power_model_terms = genModelTermsfromString(model_txt)
+true_power_model = Model(power_model_terms, ndim)
+
+print("The true model: {0}".format(true_power_model.__str__()))
+
+
+# xTrain = np.ones(ndim)
+# xTrain = np.reshape(xTrain, (nrow, ndim))
+# yTrain = power_model.evaluateModelFast(xTrain)
+# print(yTrain)
+
+print("Learning started")
+# learn the model
+learner = MLearner(budget, ndim, true_power_model)
+learned_model = learner.discover()
+
+print("Learning done!")
+print(learned_model.named_steps['linear'].coef_)
+
+learned_power_model_terms = genModelfromCoeff(learned_model.named_steps['linear'].coef_, ndim)
+learned_power_model = Model(learned_power_model_terms, ndim)
+
+print("The learned model: {0}".format(learned_power_model.__str__()))
+
+with open(os.path.join(learned_model_path, learned_model_name), 'w') as model_file:
+    model_file.write(learned_power_model.__str__())
+
+# configs = itertools.product(range(2), repeat=ndim)
+# xTest = np.zeros(shape=(2**ndim, ndim))
+# i = 0
+# for c in configs:
+#     xTest[i, :] = np.array(c)
+#     i += 1
+
+xTest = np.random.randint(2, size=(test_size, ndim))
+yTestPower = learned_model.predict(xTest)
+yTestPower_true = true_power_model.evaluateModelFast(xTest)
+
+# adding noise for the speed
+s = np.random.normal(mu, sigma, test_size)
+yTestSpeed = yTestPower_true/100 + s
+
+yDefaultPower = learned_model.predict(default_conf)
+yDefaultPower_true = true_power_model.evaluateModelFast(default_conf)
+yDefaultSpeed = yDefaultPower_true/100
+
+idx_pareto, pareto_power, pareto_speed = learner.get_pareto_frontier(yTestPower, yTestSpeed, maxX=False, maxY=True)
+json_data = get_json(pareto_power, pareto_speed)
+
+json_data_true_model = get_json([yTestPower_true[i] for i in idx_pareto], [yTestSpeed[i] for i in idx_pareto])
+
+# add the default configuration
+json_data['configurations'].append({
+    'config_id': 0,
+    'power_load': yDefaultPower[0]/3600*1000,
+    'power_load_w': yDefaultPower[0],
+    'speed': yDefaultSpeed[0]
+})
+with open(config_list_file, 'w') as outfile:
+    json.dump(json_data, outfile)
+
+json_data_true_model['configurations'].append({
+    'config_id': 0,
+    'power_load': yDefaultPower_true[0]/3600*1000,
+    'power_load_w': yDefaultPower_true[0],
+    'speed': yDefaultSpeed[0]
+})
+with open(config_list_file_true, 'w') as outfile:
+    json.dump(json_data_true_model, outfile)
+
+
--- learner/mlearner.py
+++ learner/mlearner.py
@@ -31,31 +31,24 @@ class MLearner:
     def get_pareto_frontier(self, Xs, Ys, maxX=True, maxY=True):
         # Sort the list in either ascending or descending order of X
         myList = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+        idx_sorted = sorted(range(len(Xs)), key=lambda k: Xs[k])
         # Start the Pareto frontier with the first value in the sorted list
         p_front = [myList[0]]
+        i = 0
+        pareto_idx = [idx_sorted[i]]
         # Loop through the sorted list
         for pair in myList[1:]:
+            i += 1
             if maxY:
                 if pair[1] >= p_front[-1][1]:
                     p_front.append(pair)
+                    pareto_idx.append(idx_sorted[i])
             else:
                 if pair[1] <= p_front[-1][1]:
                     p_front.append(pair)
+                    pareto_idx.append(idx_sorted[i])
         p_frontX = [pair[0] for pair in p_front]
         p_frontY = [pair[1] for pair in p_front]
-        return p_frontX, p_frontY
+        return pareto_idx, p_frontX, p_frontY
 
-    def get_json(self, pareto_power, pareto_speed):
-
-        data = {}
-        data['configurations'] = []
-        for i in range(len(pareto_power)):
-            data['configurations'].append({
-                'config_id': i + 1,
-                'power_load': pareto_power[i]/3600*1000,
-                'power_load_w': pareto_power[i],
-                'speed': pareto_speed[i]
-            })
-
-        return data
 
