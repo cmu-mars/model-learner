13fc14a7957af3e1217b5848907a85c108cb23d2
Jianhai Su <suj@email.sc.edu>
2019-10-07 10:37:11 -0400

    add online learning in the test

--- learner/testLearner.py
+++ learner/testLearner.py
@@ -8,3 +8,6 @@ model_learner.start_learning()
 if model_learner.ready.get_baseline() == AdaptationLevel.BASELINE_C:
     model_learner.dump_learned_model()
 model_learner.update_config_files()
+
+model_learner.start_online_learning()
+
