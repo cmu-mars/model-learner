82166ffa611016e2a6b0a49d8127ca4e8bf211be
Jianhai Su <jianhais@outlook.com>
2019-10-11 19:13:21 -0400

    update test for online learning

--- learner/testLearner.py
+++ learner/testLearner.py
@@ -2,16 +2,26 @@
 from learner.learn import Learn
 from learner.constants import AdaptationLevel
 
-model_learner = Learn()
-model_learner.get_true_model()
-model_learner.start_learning()
-if model_learner.ready.get_baseline() == AdaptationLevel.BASELINE_C:
-    model_learner.dump_learned_model()
 
-model_learner.update_config_files()
+try:
+    model_learner = Learn()
+    model_learner.get_true_model()
+    model_learner.start_learning()
+    if model_learner.ready.get_baseline() == AdaptationLevel.BASELINE_C:
+        model_learner.dump_learned_model()
 
-#if model_learner.ready.get_baseline() == AdaptationLevel.BASELINE_D:
-#    model_learner.start_online_learning()
-#    model_learner.update_config_files()
+    model_learner.update_config_files()
 
-
+    max_runs = 3
+    if model_learner.ready.get_baseline() == AdaptationLevel.BASELINE_D:
+        run=0
+        while model_learner.budget > model_learner.used_budget:
+            print("Online learning Run {}".format(run+1))
+            model_learner.start_online_learning()
+            model_learner.update_config_files()
+            run+=1
+            if run >= max_runs:
+                break
+    #print(model_learner.learner.measurePM(model_learner.default_conf))
+except Exception as e:
+    print("Error: {}".format(e))
