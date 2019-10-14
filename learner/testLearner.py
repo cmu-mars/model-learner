030c68fe212319a52bba05b2b0a674f5496cf804
Jianhai Su <suj@email.sc.edu>
2019-10-14 08:00:43 -0400

    update test for online learning

--- learner/testLearner.py
+++ learner/testLearner.py
@@ -12,16 +12,17 @@ try:
 
     model_learner.update_config_files()
 
-    max_runs = 3
+    max_runs = 8
     if model_learner.ready.get_baseline() == AdaptationLevel.BASELINE_D:
         run=0
         while model_learner.budget > model_learner.used_budget:
+            if run >= max_runs:
+                break
             print("Online learning Run {}".format(run+1))
             model_learner.start_online_learning()
             model_learner.update_config_files()
             run+=1
-            if run >= max_runs:
-                break
+
     #print(model_learner.learner.measurePM(model_learner.default_conf))
 except Exception as e:
     print("Error: {}".format(e))
